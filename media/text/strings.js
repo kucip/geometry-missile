930c6342374b005964ee8f892efe99c1c09a0d82
Wahyu Budiman <wahyu.budiman@impact360design.com>
2025-12-16 12:24:08 +0700

    init repo

---
+++ media/text/strings.js
@@ -0,0 +1,43 @@
+var _STRINGS = {
+	"Ad":{
+		"Mobile":{
+			"Preroll":{
+				"ReadyIn":"The game is ready in ",
+				"Loading":"Your game is loading...",
+				"Close":"Close",
+			},
+			"Header":{
+				"ReadyIn":"The game is ready in ",
+				"Loading":"Your game is loading...",
+				"Close":"Close",
+			},
+			"End":{
+				"ReadyIn":"Advertisement ends in ",
+				"Loading":"Please wait ...",
+				"Close":"Close",
+			},								
+		},
+	},
+	
+	"Splash":{
+		"Loading":"LOADING ...",	
+		"LogoLine1":"Some text here",
+		"LogoLine2":"powered by MarketJS",
+		"LogoLine3":"none",		
+        "TapToStart":"TAP TO START",				
+	},
+
+	"Game":{
+		"Settings": "SETTINGS",
+		"Pause": "PAUSED",
+		"GameOver": "GAME OVER",
+		"Score": "Score: ",
+		"Level": "Level: ",
+		"Best": "BEST: ",
+		"Click-to-play": "CLICK TO PLAY",
+	},
+
+	"Results":{
+		"Title":"High score",
+	},	
+};
